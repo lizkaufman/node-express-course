0eabe6fd0d36507b11a11d42de9bb5641dfc07ef
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-06-10 13:52:05 +0100

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 
 const PORT = 8000;
 
+const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }];
+
+app.get('/users', function (req, res) {
+  res.json({
+    success: true,
+    message: 'successfully got users. Nice!',
+    users: mockUserData,
+  });
+});
+
 app.listen(PORT, function () {
   console.log(`server is running on port ${PORT}`);
 });
